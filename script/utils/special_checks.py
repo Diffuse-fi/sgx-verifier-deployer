ec79f911e0c11e06302887a06785946a9063e0af
Aleksandr Cherenkov <aleksandr.cherenkov.3@gmail.com>
2025-02-10 09:17:07 +0000

    add neon_devnet

--- script/utils/special_checks.py
+++ script/utils/special_checks.py
@@ -1,12 +1,27 @@
 import sys
-from .network import ASSET_TESTNET
+from .network import ASSET_TESTNET, NEON_DEVNET
 
-def asset_chain_config_check(network):
+def special_check(network):
     if network == ASSET_TESTNET:
         print("asset chain does not support opcodes introduced in solidity 0.8.20")
         print("you need to change evm_version to \"paris\" is lib/p256-verifier/foundry.toml")
         print("and switch lib/risc0-ethereum submodule to")
         print("https://github.com/Diffuse-fi/risc0-ethereum/tree/release-1.3-asset-chain")
+        while(True):
+            user_input = input("If you haven't done it yet, do it and launch this script again. Resume? [y/n]: ").lower()
+            if user_input == 'y':
+                break
+            elif user_input == 'n':
+                print("cancelled execution", file=sys.stderr)
+                sys.exit(1)
+            else:
+                print("Please enter 'y' or 'n'.")
+                continue
+
+    if network == NEON_DEVNET:
+        print("neon evm does not support 0x05 precompile, you need to switch to fork")
+        print("https://github.com/Diffuse-fi/p256-verifier")
+        print('commit "replace 0x05 precompile call with solidity implementation"')
         while(True):
             user_input = input("If you haven't done it yet, do it and launch this script again. Resume? [y/n]: ").lower()
             if user_input == 'y':
