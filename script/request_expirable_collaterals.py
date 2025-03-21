046140a6ca6f7e0a41c6d16c87ca96ad7bd0f5fd
Aleksandr Cherenkov <aleksandr.cherenkov.3@gmail.com>
2025-03-21 15:12:35 +0000

    upd collateral requester

--- script/request_expirable_collaterals.py
+++ script/request_expirable_collaterals.py
@@ -1,5 +1,7 @@
 import requests
-
+import json
+from datetime import datetime, timezone
+import sys
 
 def request_tcb_info():
     pccs_url = "https://api.trustedservices.intel.com"
@@ -33,6 +35,30 @@ def process_request(collateral, req_url):
 
     object = data.split(collateral + '":')[1].split(',"signature":"')[0]
     signature = data.split(',"signature":"')[1].split('"}')[0]
+
+
+    with open("script/collaterals/" + collateral + "/object", 'r') as file:
+        existing_collateral =file.read()
+
+    nextUpdate = json.loads(existing_collateral)["nextUpdate"]
+    nextUpdate = datetime.strptime(nextUpdate, "%Y-%m-%dT%H:%M:%SZ")
+    nextUpdate = nextUpdate.replace(tzinfo=timezone.utc)
+    time_left = nextUpdate - datetime.now(timezone.utc)
+
+    print(collateral, "will expire at", nextUpdate)
+    print(time_left, "left")
+
+    while(True):
+        user_input = input("Rewrite existing collaterals? [y/n]: ").lower()
+        if user_input == 'y':
+            break
+        elif user_input == 'n':
+            print("cancelled execution", file=sys.stderr)
+            sys.exit(1)
+        else:
+            print("Please enter 'y' or 'n'.")
+            continue
+
 
     with open("script/collaterals/" + collateral + "/object", 'w') as file:
         file.write(object)
